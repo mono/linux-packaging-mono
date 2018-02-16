ad050f61f83fba09ff8b8f38953eee222f4f8a5f
Xamarin Public Jenkins (auto-signing) <releng@xamarin.com>
2018-02-16 09:15:14 +0000

    Imported Upstream version 5.13.0.129

--- mono/mini/aot-runtime.h
+++ mono/mini/aot-runtime.h
@@ -11,7 +11,7 @@
 #include "mini.h"
 
 /* Version number of the AOT file format */
-#define MONO_AOT_FILE_VERSION 142
+#define MONO_AOT_FILE_VERSION 143
 
 #define MONO_AOT_TRAMP_PAGE_SIZE 16384
 
@@ -210,7 +210,7 @@ typedef struct MonoAotFileInfo
 
 void      mono_aot_init                     (void);
 void      mono_aot_cleanup                  (void);
-gpointer  mono_aot_get_method_checked       (MonoDomain *domain,
+gpointer  mono_aot_get_method               (MonoDomain *domain,
 											 MonoMethod *method, MonoError *error);
 gpointer  mono_aot_get_method_from_token    (MonoDomain *domain, MonoImage *image, guint32 token, MonoError *error);
 gboolean  mono_aot_is_got_entry             (guint8 *code, guint8 *addr);
