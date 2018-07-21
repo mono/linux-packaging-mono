d2f372af16a5c1defb9dda5396bc497ce838c9ac
Xamarin Public Jenkins (auto-signing) <releng@xamarin.com>
2018-07-21 08:53:52 +0000

    Imported Upstream version 5.17.0.418

--- mcs/build/common/Consts.cs
+++ mcs/build/common/Consts.cs
@@ -34,11 +34,11 @@ static class Consts
 	// Use these assembly version constants to make code more maintainable.
 	//
 
-	public const string MonoVersion = "5.17.0.410";
+	public const string MonoVersion = "5.17.0.418";
 	public const string MonoCompany = "Mono development team";
 	public const string MonoProduct = "Mono Common Language Infrastructure";
 	public const string MonoCopyright = "(c) Various Mono authors";
-	public const int MonoCorlibVersion = 1051700024;
+	public const int MonoCorlibVersion = 1051700025;
 
 #if MOBILE
 	// Versions of .NET Framework for Silverlight 4.0
