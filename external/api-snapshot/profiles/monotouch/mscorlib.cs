16525e5063571f7dd3fb3a44e0ae0f663e11dcbe
Xamarin Public Jenkins (auto-signing) <releng@xamarin.com>
2018-08-04 08:57:14 +0000

    Imported Upstream version 5.17.0.535

--- external/api-snapshot/profiles/monotouch/mscorlib.cs
+++ external/api-snapshot/profiles/monotouch/mscorlib.cs
@@ -10950,7 +10950,6 @@ namespace System.Reflection
         public override string ToString() { throw null; }
     }
     [System.FlagsAttribute]
-    [System.Runtime.InteropServices.ComVisibleAttribute(true)]
     public enum ExceptionHandlingClauseOptions
     {
         Clause = 0,
