871903d6bd907597ed273317c9a9050473a8e632
Xamarin Public Jenkins (auto-signing) <releng@xamarin.com>
2018-09-15 08:57:23 +0000

    Imported Upstream version 5.19.0.381

--- external/api-snapshot/profiles/monotouch/Facades/System.Memory.cs
+++ external/api-snapshot/profiles/monotouch/Facades/System.Memory.cs
@@ -2,14 +2,14 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-[assembly:System.Reflection.AssemblyVersionAttribute("4.0.1.0")]
+[assembly:System.Reflection.AssemblyVersionAttribute("4.0.99.0")]
 [assembly:System.Diagnostics.DebuggableAttribute((System.Diagnostics.DebuggableAttribute.DebuggingModes)(2))]
 [assembly:System.Reflection.AssemblyCompanyAttribute("Mono development team")]
 [assembly:System.Reflection.AssemblyCopyrightAttribute("(c) Various Mono authors")]
 [assembly:System.Reflection.AssemblyDefaultAliasAttribute("System.Memory")]
 [assembly:System.Reflection.AssemblyDescriptionAttribute("System.Memory")]
-[assembly:System.Reflection.AssemblyFileVersionAttribute("4.0.0.0")]
-[assembly:System.Reflection.AssemblyInformationalVersionAttribute("4.0.0.0")]
+[assembly:System.Reflection.AssemblyFileVersionAttribute("4.0.99.0")]
+[assembly:System.Reflection.AssemblyInformationalVersionAttribute("4.0.99.0")]
 [assembly:System.Reflection.AssemblyProductAttribute("Mono Common Language Infrastructure")]
 [assembly:System.Reflection.AssemblyTitleAttribute("System.Memory")]
 [assembly:System.Runtime.CompilerServices.CompilationRelaxationsAttribute(8)]
