5ed59804767a58ae15ec9d1082f00e16ad59c5a0
Xamarin Public Jenkins (auto-signing) <releng@xamarin.com>
2019-05-23 09:24:40 +0000

    Imported Upstream version 6.3.0.698

--- mcs/build/common/Consts.cs
+++ mcs/build/common/Consts.cs
@@ -41,7 +41,7 @@ static partial class Consts
 	// Use these assembly version constants to make code more maintainable.
 	//
 
-	public const string MonoVersion = "6.3.0.688";
+	public const string MonoVersion = "6.3.0.698";
 	public const string MonoCompany = "Mono development team";
 	public const string MonoProduct = "Mono Common Language Infrastructure";
 	public const string MonoCopyright = "(c) Various Mono authors";
