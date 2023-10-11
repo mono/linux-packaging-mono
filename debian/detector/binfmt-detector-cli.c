62f779b5c1a7c7924fed66ed5327c6351ddef789
Pat Tullmann <pat.github@tullmann.org>
2023-10-11 15:42:46 -0700

    binfmt-detector-cli: rewrite to support PE32+ binaries
    Rewrite with hard-coded offsets into the PE file format to discern
    if a binary is PE32 or PE32+, and then to determine if it contains
    a "CLR Data Directory" entry that looks valid.
    
    Tested with PE32 and PE32+ compiled Mono binaries, PE32 and PE32+ native
    binaries, and a random assortment of garbage files.

--- debian/detector/binfmt-detector-cli.c
+++ debian/detector/binfmt-detector-cli.c
@@ -13,87 +13,169 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
+ *  Based on:
  *  binfmt PE executable helper, by Ilya Konstantinov <future@shiny.co.il>
- *  Based on PE headers structures courtesy of Mono .NET runtime project
- *  (http://www.go-mono.com).
  */
- 
+
 #include <errno.h>
-#include <string.h>
 #include <stdio.h>
-#include <locale.h>
 #include <stdlib.h>
-#include <libintl.h>
 #include <stdint.h>
 
-#include "cil-coff.h"
+#define LOG_ENABLED 0
+#define LOG(fmt, ...)					\
+	if (LOG_ENABLED) {				\
+		fprintf(stderr, fmt "\n"		\
+			__VA_OPT__(,) __VA_ARGS__ );	\
+	}
 
-//Change this to one MSDOS, MSDOS, NATIVE or CLR
-#define DETECT CLR
 
-#define _(String) gettext(String)
+/* Read a big-endian, 16-bit value at the given offset from the given file. */
+uint16_t fread_uint16(FILE* f, unsigned long offset)
+{
+	if (fseek(f, offset, SEEK_SET) != 0)
+	{
+		LOG("Failed to seek to %lu: %d", offset, errno);
+		exit(EXIT_FAILURE);
+	}
 
-/* Globals */
-enum execTypeEnum {
-	UNKNOWN,
-	MSDOS,
-	NATIVE,
-	CLR
-} execType = UNKNOWN;
+	unsigned char bytes[2];
+	if (fread(&bytes, sizeof(bytes), 1, f) != 1)
+	{
+		LOG("Failed to read %zd bytes at %lu: %d", sizeof(bytes), offset, errno);
+		exit(EXIT_FAILURE);
+	}
+
+	return bytes[0] | bytes[1] << 8;
+}
+
+
+/* Read a big-endian, 32-bit value at the given offset from the given file. */
+uint32_t fread_uint32(FILE* f, unsigned long offset)
+{
+	if (fseek(f, offset, SEEK_SET) != 0)
+	{
+		LOG("Failed to seek to %lu: %d", offset, errno);
+		exit(EXIT_FAILURE);
+	}
+
+	unsigned char bytes[4];
+	if (fread(&bytes, sizeof(bytes), 1, f) != 1)
+	{
+		LOG("Failed to read %zd bytes at %lu: %d", sizeof(bytes), offset, errno);
+		exit(EXIT_FAILURE);
+	}
+
+	return bytes[0] | bytes[1] << 8 | bytes[2] << 16 | bytes[3] << 24;
+}
+
 
 int main(int argc, char **argv)
 {
-	const char *filename;
-	FILE *image;
-	size_t read;
-	
 	if (argc < 2) exit(EXIT_FAILURE);
-	
-	filename = argv[1];
-	image = fopen(filename, "r");
-	if (image == NULL) exit(EXIT_FAILURE);
-	
-	/* Parse the MSDOS header */
-	
-	{
-		MSDOSHeader msdos_header;
-		uint32_t pe_offset;
-		read = fread(&msdos_header, sizeof(msdos_header), 1, image);
-		if (read < 1) exit(EXIT_FAILURE);
-		pe_offset = msdos_header.pe_offset[0]
-			  | msdos_header.pe_offset[1] << 8
-			  | msdos_header.pe_offset[2] << 16
-			  | msdos_header.pe_offset[3] << 24;
-		if ((pe_offset == 0) ||
-		    (fseek(image, pe_offset, SEEK_SET) != 0))
-			execType = MSDOS;
-	}
-	
-	/* Parse the PE header */
 
-	if (execType == UNKNOWN)
+	FILE* image = fopen(argv[1], "r");
+
+	if (image == NULL) exit(EXIT_FAILURE);
+
+	/*
+	 * Assume the given file is at least an MS-DOS binary (it starts
+	 * with the "MZ" magic).  Just need to disambiguate between a
+	 * CLR-managed code binary vs. a traditional native binary.
+	 */
+
+	/* Offset to the PE structured is at fixed offset 0x3C */
+	const uint32_t pe_offset = fread_uint32(image, 0x3C);
+
+	/*
+	 * PE structure should start with the "PE signature" ("PE\0\0", or
+	 * 0x00004550 in big-endian).
+	 */
+	const uint32_t pe_signature = fread_uint32(image, pe_offset);
+	if (pe_signature != 0x004550)
 	{
-		DotNetHeader dotnet_header;
-		uint16_t pe_magic;
-		uint32_t rva;
-		read = fread(&dotnet_header, sizeof(dotnet_header), 1, image);
-		if (read < 1) exit(EXIT_FAILURE);
-		pe_magic = dotnet_header.pe.pe_magic[0]
-			 | dotnet_header.pe.pe_magic[1] << 8;
-		if (dotnet_header.pesig[0] != 'P' || dotnet_header.pesig[1] != 'E' || (pe_magic != 0x10B && pe_magic != 0x20B)) exit(EXIT_FAILURE);
-		rva = dotnet_header.datadir.pe_cli_header.rva[0]
-		    | dotnet_header.datadir.pe_cli_header.rva[1] << 8
-		    | dotnet_header.datadir.pe_cli_header.rva[2] << 16
-		    | dotnet_header.datadir.pe_cli_header.rva[1] << 24;
-		if ((dotnet_header.datadir.pe_cli_header.size != 0) &&
-			(rva != 0) &&
-			(fseek(image, rva, SEEK_SET) == 0))
-			execType = CLR;
-		else
-			execType = NATIVE;
+		LOG("Wrong PE_signature (got %#08x)", pe_signature);
+		exit(EXIT_FAILURE);
 	}
+
+	/*
+	 * The PE format is stored after the 4-byte signature and 20-byte
+	 * COFF header.  There are two formats PE32 or PE32+ that might
+	 * contain a CLR-managed binary.  All other formats are native.
+	 */
+	const uint16_t pe_format = fread_uint16(image, pe_offset + 4 + 20);
+
+	/*
+	 * Compute the offset of the "CLR Runtime Header" (the 15th) entry
+	 * in the Header Data Directories section.  This offset is
+	 * computed relative to the pe_offset.
+	 */
+	size_t datadir_clr_offset = 0;
+	if (pe_format == 0x10b)
+	{
+		LOG("PE32 format");
+
+		/*
+		 * +4 bytes for pe_signature
+		 * +20 bytes for COFF header
+		 * +28 bytes for PE32 "standard fields" header
+		 * +68 bytes for "Windows-specific fields" header
+		 * +(8 * 14) bytes for the 15th entry ("CLR Runtime")
+		 *     in the Data Directories Header
+		 */
+		datadir_clr_offset = 4 + 20 + 28 + 68 + (8 * 14);
+	}
+	else if (pe_format == 0x20b)
+	{
+		LOG("PE32+ format");
+
+		/*
+		 * +4 bytes for pe_signature
+		 * +20 bytes for COFF header
+		 * +24 bytes for PE32 "standard fields" header
+		 * +88 bytes for "Windows-specific fields" header
+		 * +(8 * 14) bytes for the 15th entry ("CLR Runtime")
+		 *     in the Data Directories Header
+		 */
+		datadir_clr_offset = 4 + 20 + 24 + 88 + (8 * 14);
+	}
+	else
+	{
+		/*
+		 * This file may be in some other PE format, none of those
+		 * format support CLR-managed binaries, so we are done.
+		 */
+		LOG("Not a PE32/PE32+ binary: %#04x", pe_format);
+		exit(EXIT_FAILURE);
+	}
+
+	LOG("CLR Directory offset %#08zx", datadir_clr_offset);
+
+	/*
+	 * The CLR Data Directory entry (like all entries in the Data
+	 * Directory) consists of a 4-byte virtual address and a 4-byte
+	 * size.  (In both PE32 and PE32+ formats.)
+	 */
+	const uint32_t rva = fread_uint32(image, pe_offset + datadir_clr_offset);
+	const uint32_t rsz = fread_uint32(image, pe_offset + datadir_clr_offset + 4);
+
+	LOG("CLR Data rva=%#0x size=%#0x", rva, rsz);
+
+	/* Empty CLR Data means its a native PE executable. */
+	if ((rva == 0) || (rsz == 0))
+	{
+		LOG("No CLR content, not a Mono binary.");
+		exit(EXIT_FAILURE);
+	}
+
+	/*
+	 * At this point we assume the binary is a CLR one.  Technically, garbage
+	 * files could get this far, but hopefully the Mono runtime is the best
+	 * place to decode/explain such broken files (vs. the wine runtime).
+	 */
+
+	LOG("All checks passed.");
 	fclose(image);
 
-	/* Return a value indicating success or failure */
-	if (execType == DETECT) exit(EXIT_SUCCESS); else exit(EXIT_FAILURE);
+	exit(EXIT_SUCCESS);
 }
